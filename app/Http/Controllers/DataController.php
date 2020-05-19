1af9379bc55b48def8606373fa195dbc8c6433c5
Govinda Roy <govindaroy.ofc94@gmail.com>
2020-05-19 17:00:27 +0600

    Update

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -52,6 +52,7 @@ class DataController extends Controller
         return Showtime::join('movie_details', 'movie_showtimes.movie_id', 'movie_details.id')
                                 ->join('show_location_static', 'movie_showtimes.cinema_id', 'show_location_static.id')
                                 ->select(
+                                'movie_details.title',
                                 'movie_showtimes.id', 
                                 'movie_showtimes.cinema_id', 
                                 'movie_showtimes.date', 
