2a503f7d57fa67a48e2cf973923ddfc5c485ccd4
Govinda Roy <govindaroy.ofc94@gmail.com>
2020-05-24 20:25:14 +0600

    Update

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
@@ -18,133 +18,138 @@
 <body>
 
 
-    <div id="app">
-        <header class="movie-header">
-            <a href="#">
-                <h1 class="mr-1">{{ $movie_details->movie_title }}</h1>
-                <i class="fa fa-external-link"></i>
-            </a>
-            <select class="btn btn-secondary" style="position:relative;right:0;" id="language"
-                onchange="this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value);">
-                <option value="{{ url('/en') }}" {{ (app()->getLocale() == 'en') ? 'selected' : '' }}>English</option>
-                <option value="{{ url('/nl') }}" {{ (app()->getLocale() == 'nl') ? 'selected' : '' }}>Netherlands
-                </option>
-            </select>
-        </header>
+<div id="app">
+    <header class="movie-header">
+        <a href="#">
+            <h1 class="mr-1">{{ $movie_details->movie_title }}</h1>
+            <i class="fa fa-external-link"></i>
+        </a>
+    </header>
 
-        <div class="mobile-nav">
-            <nav role="mobile-menu">
-                <div class="menu-toggle">
-                    <input class="mobile-checkbox" type="checkbox" />
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <ul class="menu">
-                        <button class="tablinks mobile-link" onclick="openItem(event, 'getTickets')"
-                            id="defaultOpen">Get Tickets
-                        </button>
-                        <button class="tablinks mobile-link" onclick="openItem(event, 'videos')">Videos</button>
-                        <button class="tablinks mobile-link" onclick="openItem(event, 'synopsis')">Synopsis</button>
-                        <div class="sharethis-inline-share-buttons"></div>
-                    </ul>
-                </div>
-
-            </nav>
-        </div>
-
-        <div class="desktop-nav">
-            <div class="tab-wrapper">
-                <div class="tab">
-                    <button class="tablinks" onclick="openItem(event, 'getTickets')" id="defaultOpen">@lang('home.get_tickets')</button>
-                    <button class="tablinks" onclick="openItem(event, 'videos')">Videos</button>
-                    <button class="tablinks" onclick="openItem(event, 'synopsis')">@lang('home.synopsis')</button>
+    <div class="mobile-nav">
+        <nav role="mobile-menu">
+            <div class="menu-toggle">
+                <input class="mobile-checkbox" type="checkbox"/>
+                <span></span>
+                <span></span>
+                <span></span>
+                <ul class="menu">
+                    <button class="tablinks mobile-link" onclick="openItem(event, 'getTickets')" id="defaultOpen">Get Tickets
+                    </button>
+                    <button class="tablinks mobile-link" onclick="openItem(event, 'videos')">Videos</button>
+                    <button class="tablinks mobile-link" onclick="openItem(event, 'synopsis')">Synopsis</button>
                     <div class="sharethis-inline-share-buttons"></div>
-                </div>
+                </ul>
             </div>
 
+        </nav>
+    </div>
+
+    <div class="desktop-nav">
+        <div class="tab-wrapper">
+            <div class="tab">
+                <button class="tablinks" onclick="openItem(event, 'getTickets')" id="defaultOpen">Get Tickets</button>
+                <button class="tablinks" onclick="openItem(event, 'videos')">Videos</button>
+                <button class="tablinks" onclick="openItem(event, 'synopsis')">Synopsis</button>
+                <div class="sharethis-inline-share-buttons"></div>
+            </div>
         </div>
 
+    </div>
 
-        <div id="getTickets" class="tabcontent">
-            <div class="grid">
-                <section class="movie-poster">
-                    <img src="{{ $movie_details->image1 }}" alt="">
-                </section>
 
-                <section class="movie-content">
-                    <div class="movie-thumb">
-                        <button role="button">
-                            <img src="" alt="">
-                        </button>
+    <div id="getTickets" class="tabcontent">
+        <div class="grid">
+            <section class="movie-poster">
+                <img src="{{ $movie_details->image1 }}"
+                     alt="">
+            </section>
+
+            <section class="movie-content">
+                <div class="movie-thumb">
+                    <button role="button">
+                        <img
+                            src=""
+                            alt="">
+                    </button>
+                </div>
+
+                <div class="land-content">
+
+                    <form class="landing-search">
+                        <div class="form-group position-relative">
+                            <input id="land-search-input" type="search" class="form-control q" autocomplete="off"
+                                   placeholder="Search">
+                            <button type="submit" id="land-search-btn">&times;</button>
+                        </div>
+                    </form>
+
+
+                    <div class="land-search-result">
+                        <p class="ls-help">ENTER YOUR LOCATION ABOVE OR SELECT YOUR THEATER BELOW</p>
+
+                        <div class="main-acc accordion" id="accordionExample"></div>
+
+                        <div class="city-acc accordion d-none" id="accordionExample2"></div>
+
                     </div>
 
-                    <div class="land-content">
+                    <br>
 
-                        <form class="landing-search">
-                            <div class="form-group position-relative">
-                                <input id="land-search-input" type="search" class="form-control q" autocomplete="off"
-                                    placeholder="Search">
-                                <button type="submit" id="land-search-btn">&times;</button>
-                            </div>
-                        </form>
+                    <p class="text-center">WATCH THE TRAILER BELOW
+                    </p>
 
-
-                        <div class="land-search-result">
-                            <p class="ls-help">@lang('home.enter_location')</p>
-
-                            <div class="main-acc accordion" id="accordionExample"></div>
-
-                            <div class="city-acc accordion d-none" id="accordionExample2"></div>
-
-                        </div>
-
-                        <br>
-
-                        <p class="text-center">@lang('home.watch_trailer')
-                        </p>
-
-                        <iframe class="w-100" height="200" src="{{ $youtube_url }}" frameborder="0"
+                    <iframe class="w-100" height="200" src="{{ $youtube_url }}" frameborder="0"
                             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                             allowfullscreen></iframe>
 
 
-                        <h2 class="h6 my-3">@lang('home.more_cities')</h2>
+                    <h2 class="h6 my-3">MORE SHOWTIMES FOUND IN THE CITIES BELOW</h2>
 
-                        <ul class="landing-city-list"></ul>
-                    </div>
+                    <ul class="landing-city-list"></ul>
+                </div>
 
-                </section>
-                <section class="movie-details">
-                    <img width="100" class="d-block mx-auto" src="{{ $movie_details->image1 }}" alt="">
+            </section>
+            <section class="movie-details">
+                <img width="100" class="d-block mx-auto" src="{{ $movie_details->image1 }}"
+                     alt="">
 
-                    <h3 class="underline text-center my-3">
-                        {{ $movie_details->movie_description_short }}
-                    </h3>
+                <h3 class="underline text-center my-3">
+                    {{ $movie_details->movie_description_short }}
+                </h3>
 
-                    <p class="excerpt mt-3">
-                        {{ $movie_details->movie_description_long }}
+                <p class="excerpt mt-3">
+                    {{ $movie_details->movie_description_long }}
+                </p>
+
+                <div class="synopsis-meta">
+                    <p>
+                        <span>Directed by:</span> {{ $movie_details->director }}
                     </p>
 
-                    <div class="synopsis-meta">
-                        <p>
-                            <span>Directed by:</span> {{ $movie_details->director }}
-                        </p>
+                    <p>
+                        <span>Written by:</span> {{ $movie_details->writer }}
+                    </p>
 
-                        <p>
-                            <span>Written by:</span> {{ $movie_details->writer }}
-                        </p>
+                    <p>
+                        <span>Produced by:</span> {{ $movie_details->producer }}
+                    </p>
 
-                        <p>
-                            <span>Produced by:</span> {{ $movie_details->producer }}
-                        </p>
+                    <p>
+                        <span>Cast:</span> {{ $movie_details->actors }}
+                    </p>
 
-                        <p>
-                            <span>Cast:</span> {{ $movie_details->actors }}
-                        </p>
+                    <p>
+                        <span>Rating:</span> {{ $rating }}
+                    </p>
 
-                        <p>
-                            <span>Rating:</span> {{ $rating }}
-                        </p>
+                    <p>
+                        <span>Duration:</span> {{ $movie_details->duration }}
+                    </p>
+                </div>
+            </section>
+        </div>
+    </div>
 
     <div id="videos" class="tabcontent">
         <div class="iframe-containerr">
@@ -153,25 +158,19 @@
                     allowfullscreen>
             </iframe>
         </div>
-
-        <div id="videos" class="tabcontent">
-            <div class="iframe-container">
-                <iframe width="560" height="315" src="{{ $youtube_url }}" frameborder="0"
-                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-                </iframe>
-            </div>
         </div>
     </div>
 
     <div id="synopsis" class="tabcontent">
         <div class="synopsis-details">
 
-            <img src="{{ $movie_details->image1 }}" alt="">
+            <img src="{{ $movie_details->image1 }}"
+                 alt="">
 
             <div class="synopsis-grid mt-5">
-
+                
                 <div class="excerpt">
-                    <h3 class="underline mb-3"> {{ $movie_details->movie_description_short }}</h3>
+                <h3 class="underline mb-3"> {{ $movie_details->movie_description_short }}</h3>
                     {{ $movie_details->movie_description_long }}
                 </div>
 
